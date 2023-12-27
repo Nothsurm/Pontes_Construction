e5586e4573ba74e638b1e85252e0833f5444be58
Mike Rushton <mrush94ton@gmail.com>
2023-12-27 17:22:39 +0000

    created pages and page routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Testimonial from "./pages/Testimonial";
+import Contact from "./pages/Contact";
 
 export default function App() {
   return (
-    <div className='text-red-500 font-bold'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/contact' element={<Contact />}></Route>
+        <Route path='/testimonial' element={<Testimonial />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
